1f628e429a02fa75fc28fa652fd8ba75ad83e531
Shivam Kalra <shiv_kalra@yahoo.com>
2012-03-05 02:04:43 -0500

    Returning system exit status.

--- src/cuidvd/DVDRentalSystem.java
+++ src/cuidvd/DVDRentalSystem.java
@@ -41,6 +41,7 @@ public class DVDRentalSystem {
 		if (args.length != 2) {
 			System.out
 					.println("Transaction file and Current DVD file is not provided as command line arguments.");
+			System.exit(1);
 			return;
 		}
 
@@ -87,6 +88,7 @@ public class DVDRentalSystem {
 
 			} catch (Exception ex) {
 				System.err.println("Error: " + ex.getMessage());
+				System.exit(1);
 			}
 		}
 	}
