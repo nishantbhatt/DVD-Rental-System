1ae4fb4f6defc99a373f531667e08b6050918e95
Nishant Bhatt <nishant@live.ca>
2012-03-30 21:03:17 -0400

    Adding test cases for merge.

---
+++ src/test/backend/BackEndEngineTest.java
@@ -0,0 +1,158 @@
+package test.backend;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import junit.framework.JUnit4TestAdapter;
+
+import org.junit.Test;
+
+import session.backend.BackEndEngine;
+import session.backend.FatalBackEndException;
+import session.backend.FileType;
+
+public class BackEndEngineTest {
+
+	public static void main(String... args) {
+		junit.textui.TestRunner.run(suite());
+	}
+
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(BackEndEngineTest.class);
+	}
+
+	@Test
+	public void testMerge1() throws URISyntaxException, IOException {
+		/* load the file from the resource */
+		File t1 = new File(BackEndEngineTest.class.getResource(
+				"test-cases/merge/test1/t1.tf").toURI());
+
+		/* load second transaction file */
+		File t2 = new File(BackEndEngineTest.class.getResource(
+				"test-cases/merge/test1/t2.tf").toURI());
+
+		/* load expected transaction file */
+		File t3 = new File(BackEndEngineTest.class.getResource(
+				"test-cases/merge/test1/mtf.ex").toURI());
+
+		/* create temporary file for actual transaction file */
+		File mtf = File.createTempFile("mtf", "tf");
+		mtf.createNewFile();
+
+		/* merge the result */
+		BackEndEngine.merge(
+				new String[] { t1.getAbsolutePath(), t2.getAbsolutePath() },
+				mtf.getAbsolutePath());
+
+		/* assert that expected output matches with actual output */
+		assertReaders(new BufferedReader(new FileReader(t3)),
+				new BufferedReader(new FileReader(mtf)));
+	}
+
+	/* When merged transaction file could not be written */
+	@Test
+	public void testMerge2() throws URISyntaxException, IOException {
+		/* load the file from the resource */
+		File t1 = new File(BackEndEngineTest.class.getResource(
+				"test-cases/merge/test2/t1.tf").toURI());
+
+		/* load second transaction file */
+		File t2 = new File(BackEndEngineTest.class.getResource(
+				"test-cases/merge/test2/t2.tf").toURI());
+		
+		/* create temporary file for actual transaction file */
+		File mtf = File.createTempFile("mtf", "tf");
+		mtf.setWritable(false);
+		mtf.deleteOnExit();
+
+		/* merge the result */
+		try {
+			BackEndEngine
+					.merge(new String[] { t1.getAbsolutePath(),
+							t2.getAbsolutePath() }, mtf.getAbsolutePath());
+		} catch (FatalBackEndException ex) {
+			/*
+			 * Fatal Back End Exception should corresponds to Merged Transaction
+			 * File only
+			 */
+			if (ex.getFileType() != FileType.MergedTransactionFile)
+				throw ex;
+		}
+	}
+
+	/* When one of the transaction file is not READABLE or NOT EXISTS */
+	@Test
+	public void testMerge3() throws IOException, URISyntaxException {
+		/* load the file from the resource (NOT WRITABLE OR READABLE) */
+		File t1 = File.createTempFile("trf", "tf");
+		t1.setReadable(false);
+		t1.deleteOnExit();
+
+		/* load second transaction file */
+		File t2 = new File(BackEndEngineTest.class.getResource(
+				"test-cases/merge/test3/t2.tf").toURI());
+
+		/* create temporary file for actual transaction file */
+		File mtf = File.createTempFile("mtf", "tf");
+
+		try {
+			BackEndEngine
+					.merge(new String[] { t1.getAbsolutePath(),
+							t2.getAbsolutePath() }, mtf.getAbsolutePath());
+		} catch (FatalBackEndException ex) {
+			/*
+			 * Fatal Back End Exception should corresponds to Merged Transaction
+			 * File only
+			 */
+			if (ex.getFileType() != FileType.TransactionFile)
+				throw ex;
+		}
+	}
+
+	/* When one of the transaction file has improper formats */
+	@Test
+	public void testMerge4() throws IOException, URISyntaxException {
+		/* load the file from the resource (NOT WRITABLE OR READABLE) */
+		File t1 = new File(BackEndEngineTest.class.getResource(
+				"test-cases/merge/test4/t1.tf").toURI());
+
+		/* load second transaction file */
+		File t2 = new File(BackEndEngineTest.class.getResource(
+				"test-cases/merge/test4/t2.tf").toURI());
+
+		/* create temporary file for actual transaction file */
+		File mtf = File.createTempFile("mtf", "tf");
+
+		try {
+			BackEndEngine
+					.merge(new String[] { t1.getAbsolutePath(),
+							t2.getAbsolutePath() }, mtf.getAbsolutePath());
+		} catch (FatalBackEndException ex) {
+			/*
+			 * Fatal Back End Exception should corresponds to Merged Transaction
+			 * File only
+			 */
+			if (ex.getFileType() != FileType.TransactionFile)
+				throw ex;
+		}
+	}
+
+	public static void assertReaders(BufferedReader expected,
+			BufferedReader actual) throws IOException {
+		String expectedLine;
+		while ((expectedLine = expected.readLine()) != null) {
+			String actualLine = actual.readLine();
+			assertNotNull("Expected had more lines then the actual.",
+					actualLine);
+			assertEquals(expectedLine, actualLine);
+		}
+
+		assertNull("Actual had more lines then the expected.",
+				actual.readLine());
+	}
+}
